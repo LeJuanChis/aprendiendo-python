5960082b17d82621d847c85030345b856e9d36cf
Juan Carlos Osorio <juancaom12@gmail.com>
2023-03-14 10:25:25 -0500

    Nueva funcionalidad alexa camera

--- webPractice/main.py
+++ webPractice/main.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template
 import cv2
+import imutils
+import time
 
 vid = cv2.VideoCapture(0)
-
+print(vid.open(0))
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def main():
 
-    while (True):
+    while True:
         res, frame = vid.read()
+        frame = imutils.resize(frame, width=640)
         cv2.rectangle(frame, (100, 100), (200, 200), (255, 0, 0), 2)
 
         cv2.imshow('frame', frame)
@@ -17,8 +20,7 @@ def main():
         if cv2.waitKey(1) & 0xff == ord('q'):
             break
 
-        vid.release()
-        cv2.destroyAllWindows()
+
     return render_template("index.html")
 
 if __name__ == '__main__':
